b60ee9ae732b4545c28bb224d40facbc57b8cfdb
Hristo Chakarov <mail@ickata.net>
2015-04-27 20:34:18 +0300

    Validators - implementation

--- 05. Regexp/validator_email.js
+++ 05. Regexp/validator_email.js
@@ -4,5 +4,8 @@ if ( typeof module != 'undefined' ) {
 }
 
 function validateEmail( email ) {
-   return false;
+   // TODO: Find a way to match "something.something" as valid,
+   // but detect "something..something" as invalid, with regex only.
+   var match = /^[^\.]([“”\s\w\-\.\+]{1,})[“”\s\w\-\+]{2}@(?:(?:[a-z\d][a-z\d\-]+\.)+(?:(?!web)[a-z]{2,8})|(?:(\[?\d{1,3})\.){3}\2\]?)$/i.exec( email );
+   return match && match[1].indexOf('..') == -1 ? true : false;
 }
--- 05. Regexp/validator_phone.js
+++ 05. Regexp/validator_phone.js
@@ -4,5 +4,5 @@ if ( typeof module != 'undefined' ) {
 }
 
 function validatePhone( phone ) {
-   return false;
+   return /^(\+\d{3}\s?)?\(?\d{2,}[\d\-\s\/\)]{5,}$/.test( phone );
 }
